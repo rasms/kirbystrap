1bf3f527e60d830af7c54f8d8e875cf3ad112352
Rasmus Leissner <rleissner@irhh.de>
2015-02-09 15:39:22 +0100

    added submenu and templates

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -14,11 +14,26 @@
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <?php foreach($pages->visible() as $p): ?>
+              <?php if($p->hasVisibleChildren()): ?>
+                <li class="dropdown">
+                  <a <?php e($p->isOpen(), ' class="active')?> class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?> <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <?php foreach($p->children()->visible() as $p): ?>
+                    <li>
+                      <a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
+                    </li>
+                    <?php endforeach ?>
+                  </ul>
+                </li>
+                  
+                  <?php else: ?>
               <li>
-                <a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
+                 <a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a> 
               </li>
+              <?php endif ?>
             <?php endforeach ?>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
     </nav>
+
